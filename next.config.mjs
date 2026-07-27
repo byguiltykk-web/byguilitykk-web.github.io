61a889715ed4f05e9a1d3c4d705f621c7e50ada5
byguiltykk-web <byguiltykk@gmail.com>
2026-07-27 17:51:45 +0200

    Correction affichage GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = 'byguilitykk.github.io';
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+
+  basePath: isProduction ? `/${repoName}` : '',
+  assetPrefix: isProduction ? `/${repoName}/` : '',
+
   images: {
     unoptimized: true,
   },
